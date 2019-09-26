8ec09a1554a666475bd1dbd20ca5c005207774b7
CHeGIVaRO1 <fantomscorpion@gmail.com>
2019-09-26 16:44:08 +0400

    add pages, categori, rubric, and add views

--- lenta/urls.py
+++ lenta/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, by_rubric, by_category
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('rubric/<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('categori/<int:categori_id>', by_category, name='by_categori'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- lenta/views.py
+++ lenta/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
 from .models import Rubric, Categori, News
+
+
 def index(request):
     rubrics = Rubric.objects.all()
     categories = Categori.objects.all()
@@ -15,3 +15,26 @@ def index(request):
             if rubric.name == categori.rubric.name:
                 print(type(categori.rubric))
     return render(request, 'index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    newses = News.objects.filter(rubric=rubric_id)
+    categories = Categori.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {"rubrics": rubrics,
+               "categories": categories,
+               "newses": newses
+               }
+    return render(request, 'by_rubric.html', context)
+
+
+def by_category(request, categori_id):
+    newses = News.objects.filter(categori=categori_id)
+    categories = Categori.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {"rubrics": rubrics,
+               "categories": categories,
+               "newses": newses
+               }
+    return render(request, 'by_categori.html', context)
+
